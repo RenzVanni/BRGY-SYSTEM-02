3f6bf1be7c3d271accfe34d206e7ecfacf2fd303
Renz <renzvanni.dev@gmail.com>
2025-10-31 04:33:26 +0800

    remove custom dialog box

--- src/app/(Main)/Account/page.tsx
+++ src/app/(Main)/Account/page.tsx
@@ -1,17 +1,8 @@
 'use client';
 import { accountColumn } from '@/config/account/accountColumnDef';
 import { DataTable } from '@/components/table/data-table';
-import React, { useContext, useEffect, useState } from 'react';
-import { LOGIN } from '@/constants/navigation';
-import { AccountColumnModel, AccountType } from '@/types/accountType';
-import { useRouter } from 'next/navigation';
-import { ContextTheme } from '@/config/config_context';
-import { useAccounts, usePaginate } from '@/hooks/useQuery';
-import CustomDialog from '@/components/CustomDialog';
-import FormDialog from '@/components/FormDialog/ResidentFormDialog';
-import { paginateAccountsApi } from '@/app/api/accountApi';
+import React from 'react';
 import AccountFormDialog from '@/components/FormDialog/AccountFormDialog';
-import { accountMapperForData } from '@/hooks/mapper';
 import { ACCOUNT_PATH } from '@/constants/Backend_Slugs';
 import { apiHooks } from '@/hooks/apiHooks';
 
--- src/app/(Main)/Residents/page.tsx
+++ src/app/(Main)/Residents/page.tsx
@@ -1,19 +1,10 @@
 'use client';
-import { formatFetchedResidents } from '@/app/api/resident_api';
-import { paginateResidentsApi } from '@/app/api/residentApi';
-import CustomDialog from '@/components/CustomDialog';
 import ResidentFormDialog from '@/components/FormDialog/ResidentFormDialog';
 import { DataTable } from '@/components/table/data-table';
-import { ContextTheme } from '@/config/config_context';
 import { residentColumn } from '@/config/residents/residentsColumnsDef';
 import { RESIDENTS_PATH } from '@/constants/Backend_Slugs';
-import { LOGIN } from '@/constants/navigation';
 import { apiHooks } from '@/hooks/apiHooks';
-import { mapResidents } from '@/hooks/mapper';
-import { usePaginate, useResidents } from '@/hooks/useQuery';
-import { ResidentColumnModel, ResidentType } from '@/types/residentsType';
-import { useRouter } from 'next/navigation';
-import React, { useContext, useEffect, useState } from 'react';
+import React from 'react';
 
 const page = () => {
   const { paginateResidentsHook } = apiHooks(RESIDENTS_PATH);
@@ -23,34 +14,6 @@ const page = () => {
       <DataTable columns={residentColumn} data={payload} pages={pages} />
 
       <ResidentFormDialog />
-
-      {/* {isResidentPresent && (
-        <CustomDialog
-          isOpen={isAddResident}
-          setIsOpen={setIsAddResident}
-          is_Add_Resident={true}
-          whatsType="create"
-        />
-      )}
-
-      {isResidentPresent && (
-        <CustomDialog
-          data={residentData}
-          isOpen={isEditResident}
-          setIsOpen={setIsEditResident}
-          is_Edit_Resident={true}
-          whatsType="edit"
-        />
-      )}
-
-      {isResidentPresent && (
-        <CustomDialog
-          data={residentData}
-          isOpen={isCreateCertificate}
-          setIsOpen={setIsCreateCertificate}
-          is_Create_Certificate={true}
-        />
-      )} */}
     </>
   );
 };
